8c7589b81c5d9bd825b31536419c47ba7dcd0fc5
shiqian <liuguangqi@xiaohongshu.com>
2022-03-22 17:25:22 +0800

    feat: quick commit

--- lua/plugin-config/coc.lua
+++ lua/plugin-config/coc.lua
@@ -1,25 +1,25 @@
-vim.g.coc_global_extensions = [
-  \ 'coc-html',
-  \ 'coc-json',
-  \ 'coc-css',
-  \ 'coc-tsserver',
-  \ 'coc-lists',
-  \ 'coc-gitignore',
-  \ 'coc-vimlsp',
-  \ 'coc-lists',
-  \ 'coc-git',
-  \ 'coc-sourcekit',
-  \ 'coc-highlight',
-  \ 'coc-snippets',
-  \ 'coc-eslint',
-  \ 'coc-prettier',
-  \ 'coc-webpack',
-  \ 'coc-spell-checker',
-  \ 'coc-texlab',
-  \ 'coc-cssmodules',
-  \ 'coc-pairs',
-  \ 'coc-styled-components'
-]
+vim.g.coc_global_extensions = {
+   'coc-html',
+   'coc-json',
+   'coc-css',
+   'coc-tsserver',
+   'coc-lists',
+   'coc-gitignore',
+   'coc-vimlsp',
+   'coc-lists',
+   'coc-git',
+   'coc-sourcekit',
+   'coc-highlight',
+   'coc-snippets',
+   'coc-eslint',
+   'coc-prettier',
+   'coc-webpack',
+   'coc-spell-checker',
+   'coc-texlab',
+   'coc-cssmodules',
+   'coc-pairs',
+   'coc-styled-components'
+}
 
 vim.api.nvim_set_keymap("n", "<leader>rn", "<Plug>(coc-rename)", {})
 vim.api.nvim_set_keymap("n", "<leader><leader>", ":CocAction", {})
