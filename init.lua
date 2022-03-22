ae88d52aa11a744e1b6b4d897d98ac52f8025919
shiqian <liuguangqi@xiaohongshu.com>
2022-03-22 16:43:35 +0800

    feat: quick commit

---
+++ init.lua
@@ -0,0 +1,14 @@
+-- 基础配置
+require("basic")
+-- Packer插件管理
+require("plugins")
+-- 快捷键映射
+require("keybindings")
+-- 主题设置
+require("colorscheme")
+
+-- 插件配置
+require("plugin-config.nvim-tree")
+require("plugin-config.lualine")
+require("plugin-config.nvim-treesitter")
+require("plugin-config.surround")
