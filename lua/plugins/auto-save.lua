243859cf3628e9dd0c2b7032c893d4cf7d54498c
shiqian <liuguangqi@xiaohongshu.com>
2023-12-29 14:36:17 +0800

    feat: tree shaking

---
+++ lua/plugins/auto-save.lua
@@ -0,0 +1,18 @@
+return {
+  "pocco81/auto-save.nvim",
+  opts = {
+    enabled = true,
+    execution_message = {
+      message = function() -- message to print on save
+        return ("◝(⑅•ᴗ•⑅)◜..°♡  autoSave: saved at" .. vim.fn.strftime("%H:%M:%S"))
+      end,
+      dim = 0.18, -- dim the color of `message`
+      cleaning_interval = 1000, -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
+    },
+    events = { "InsertLeave", "TextChanged" },
+    write_all_buffers = false,
+    on_off_commands = true,
+    clean_command_line_interval = 0,
+    debounce_delay = 1 * 500,
+  },
+}
