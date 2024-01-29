78649a9fd7a8e9e0f48a8ea0440a946f7ab517f1
shiqian <liuguangqi@xiaohongshu.com>
2024-01-29 16:21:18 +0800

    feat: quick commit

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -18,9 +18,27 @@ return {
     window = {
       position = "right",
       mappings = {
-        o = "open",
+        o = "open_with_window_picker",
         S = "split_with_window_picker",
         s = "vsplit_with_window_picker",
+        -- y = {
+        --   "copy",
+        --   config = {
+        --     show_path = "none", -- "none", "relative", "absolute"
+        --   },
+        -- },
+        -- Y = {
+        --   "copy",
+        --   config = {
+        --     show_path = "none", -- "none", "relative", "absolute"
+        --   },
+        -- },
+        -- gy = {
+        --   "copy",
+        --   config = {
+        --     show_path = "absolute", -- "none", "relative", "absolute"
+        --   },
+        -- },
       },
     },
   },
