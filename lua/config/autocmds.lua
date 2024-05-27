d9b4301d8d4ff46426472e0a5b3f406a7329632d
liuguangqi <liuguangqi@xiaohongshu.com>
2024-05-27 15:00:25 +0800

    feat: quick commit

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -8,3 +8,13 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     vim.b.autoformat = false
   end,
 })
+
+vim.api.nvim_create_user_command("DiagnosticToggle", function()
+	local config = vim.diagnostic.config
+	local vt = config().virtual_text
+	config {
+		virtual_text = not vt,
+		underline = not vt,
+		signs = not vt,
+	}
+end, { desc = "toggle diagnostic" })
