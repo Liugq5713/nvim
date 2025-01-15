5c174be30dc7f1dc9c621961a5b9303b73fe0fdd
liuguangqi <liuguangqi@xiaohongshu.com>
2025-01-15 14:30:32 +0800

    feat: quick commit

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -3,15 +3,15 @@ return {
   opts = {
     enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
     trigger_events = { -- See :h events
-      immediate_save = { "BufLeave", "FocusLost", "ExitPre", "QuitPre", "VimSuspend" }, -- vim events that trigger an immediate save
-      defer_save = { "CursorHold" }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
+      immediate_save = { "ExitPre", "QuitPre", "VimSuspend" }, -- vim events that trigger an immediate save
+      defer_save = { "CursorHold", "BufLeave", "FocusLost" }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
       cancel_deferred_save = { "InsertEnter" }, -- vim events that cancel a pending deferred save
     },
     condition = nil,
     write_all_buffers = false, -- write all buffers when the current one meets `condition`
     noautocmd = false, -- do not execute autocmds when saving
     lockmarks = false, -- lock marks when saving, see `:h lockmarks` for more details
-    debounce_delay = 3000, -- delay after which a pending save is executed
+    debounce_delay = 2000, -- delay after which a pending save is executed
     -- log debug messages to 'auto-save.log' file in neovim cache directory, set to `true` to enable
     debug = false,
   },
